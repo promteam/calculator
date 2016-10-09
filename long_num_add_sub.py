b0bc8636b55e96cd9e89a75dcb854660ff573d05
chaykovskiy-ilya <chaykovskiy.ilya@gmail.com>
2016-10-09 18:57:29 +0300

    Minor bugs fixes, interface menu changes

--- long_num_add_sub.py
+++ long_num_add_sub.py
@@ -1,5 +1,3 @@
-# -*- coding: utf8 -*-
- 
 def long_multiply(a, b):
     a.reverse()
     b.reverse()
@@ -14,30 +12,36 @@ def long_multiply(a, b):
         c[i] %= 10
     while len(c) > 1 and c[len(c) - 1] == 0:
         c.pop()
-    print ''.join([str(i) for i in reversed(c)]),
- 
- 
+    c.reverse()
+    print ''.join([str(i) for i in c])
+
+
 def long_num_add_sub(a, b, f):
     Ans = []
-    _lesseq = True
+    _less = True
     if not(f):
         if (len(a) > len(b)):
-            _lesseq = False
+            _less = False
         elif len(a) == len(b):
             for i in range(len(a)):
                 if a[i] > b[i]:
-                    _lesseq = False
+                    _less = False
                     break
-            _lesseq = False
+            _less = False
     a.reverse()
     b.reverse()
+    a += [0] * max(len(b) - len(a), 0)
+    b += [0] * max(len(a) - len(b), 0)
     sign = True
-    if _lesseq:
+    if _less:
         sign = False
         a, b = b, a
     if f:
-        for i in range(min(len(a), len(b))):
-            Ans.append(a[i] + b[i])
+        Ans = [0] * max(len(a), len(b))
+        for i in range(len(a)):
+            Ans[i] += a[i]
+        for i in range(len(b)):
+            Ans[i] += b[i]
         for i in range(len(Ans) - 1):
             Ans[i + 1] += Ans[i] // 10
             Ans[i] %= 10
@@ -60,44 +64,50 @@ def long_num_add_sub(a, b, f):
             Ans.append('-')
         Ans.reverse()
     print ''.join(map(str, Ans))
- 
- 
+
+
 def long_arithmetics():
-    print "Доступные пункты меню:"
-    print "Введите 1 для сложения или вычитания длинных чисел."
-    print "Введите 2 для умножение длинных чисел."
-    f = raw_input()
-    if not(f.isdigit()):
-        print "Неверный формат ввода!"
+    while True:
+        print "Доступные пункты меню:"
+        print "Введите 1 для сложения или вычитания длинных чисел."
+        print "Введите 2 для умножения длинных чисел."
+        f = raw_input()
+        if not(f.isdigit()):
+            print "Неверный формат ввода!\n"
+            continue
+        f = int(f)
+        if f != 1 and f != 2:
+            print "Неверный формат ввода!\n"
+            continue
+
+        if f == 1:
+            while True:
+                print "Введите два неотрицательных числа в формате: [число] (+/-) [число]"
+                A = map(str, raw_input().split())
+                if len(A) != 3 or not (A[0].isdigit()) or A[1] != '+' and A[1] != '-' or not(A[2].isdigit()) or not(A[2][0].isdigit()) or not(A[0][0].isdigit()):
+                    print "Неверный формат ввода!\n"
+                    continue
+                a = []
+                b = []
+                for i in range(len(A[0])):
+                    a.append(int(A[0][i]))
+                for i in range(len(A[2])):
+                    b.append(int(A[2][i]))
+                long_num_add_sub(a, b, A[1] == '+')
+                return
+        elif f == 2:
+            while True:
+                print "Введите два неотрицательных числа в формате: [число] * [число]"
+                A = map(str, raw_input().split())
+                if len(A) != 3 or not(A[0].isdigit()) or A[1] != '*' or not(A[2].isdigit()) or not(A[2][0].isdigit()) or not(A[0][0].isdigit()):
+                    print "Неверный формат ввода!\n"
+                    continue
+                a = []
+                b = []
+                for i in range(len(A[0])):
+                    a.append(int(A[0][i]))
+                for i in range(len(A[2])):
+                    b.append(int(A[2][i]))
+                long_multiply(a, b)
+                return
         return
-    f = int(f)
-    if f != 1 and f != 2:
-        print "Неверный формат ввода!"
-        return
-    if f == 1:
-        print "Введите два числа в формате: [число] (+/-) [число]"
-        A = map(str, raw_input().split())
-        if len(A) != 3 or not (A[0].isdigit()) or A[1] != '+' and A[1] != '-' or not(A[2].isdigit()) or not(A[2][0].isdigit()) or not(A[0][0].isdigit()):
-            print "Неверный формат ввода!"
-            return
-        a = []
-        b = []
-        for i in range(len(A[0])):
-            a.append(int(A[0][i]))
-        for i in range(len(A[2])):
-            b.append(int(A[2][i]))
-        long_num_add_sub(a, b, A[1] == '+')
-    elif f == 2:
-        print "Введите два числа в формате: [число] * [число]"
-        A = map(str, raw_input().split())
-        if len(A) != 3 or not(A[0].isdigit()) or A[1] != '*' or not(A[2].isdigit()) or not(A[2][0].isdigit()) or not(A[0][0].isdigit()):
-            print "Неверный формат ввода!"
-            return
-        a = []
-        b = []
-        for i in range(len(A[0])):
-            a.append(int(A[0][i]))
-        for i in range(len(A[2])):
-            b.append(int(A[2][i]))
-        long_multiply(a, b)
-    return
